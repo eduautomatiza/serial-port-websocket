066f86766c9d1081ebd2bdcdbae6c33c890fcfb8
Eduardo Guse <eduautomatiza@yahoo.com.br>
2023-07-03 21:20:30 -0300

    arquivo de configuração

--- serialworker.py
+++ serialworker.py
@@ -1,19 +1,35 @@
 import serial
 import time
 import multiprocessing
-
-# Change this to match your local settings
-SERIAL_PORT = '/dev/ttyUSB0'
-SERIAL_BAUDRATE = 115200
-
+from datetime import datetime
 
 class SerialProcess(multiprocessing.Process):
-
-    def __init__(self, input_queue, output_queue):
+    def __init__(
+        self,
+        input_data_queue,
+        output_data_queue,
+        input_control_queue,
+        output_control_queue,
+        config,
+    ):
         multiprocessing.Process.__init__(self)
-        self.input_queue = input_queue
-        self.output_queue = output_queue
-        self.sp = serial.Serial(SERIAL_PORT, SERIAL_BAUDRATE, timeout=1)
+        self.input_data_queue = input_data_queue
+        self.output_data_queue = output_data_queue
+        self.input_control_queue = input_control_queue
+        self.output_control_queue = output_control_queue
+        self.port = config["serial_port"]["port"]
+        self.baudRate = config["serial_port"]["baudrate"]
+        self.sp = serial.Serial(self.port, self.baudRate, timeout=1)
+        if self.sp.is_open:
+            print(
+                "success opening port",
+                config["serial_port"]["port"],
+            )
+        else:
+            print(
+                "failed to open port",
+                config["serial_port"]["port"],
+            )
 
     def close(self):
         self.sp.close()
@@ -26,22 +42,23 @@ class SerialProcess(multiprocessing.Process):
         return self.sp.readline()
 
     def run(self):
-
         self.sp.flushInput()
 
         while True:
             # look for incoming tornado request
-            if not self.input_queue.empty():
-                data = self.input_queue.get()
+            if not self.input_data_queue.empty():
+                data = self.input_data_queue.get()
                 # send it to the serial device
                 self.writeSerial(data)
-                print("2-writing to serial: " + data)
+                print("2-writing to serial: ", data)
 
             # look for incoming serial data
             n = self.sp.inWaiting()
-            if (n > 0):
-                print("read serial n bytes: " + str(n))
+            if n > 0:
+                print("read serial n bytes: ", n)
                 data = self.readSerial(n)
-                print("reading from serial: " + str(data))
+                print("reading from serial: ", data)
                 # send it back to tornado
-                self.output_queue.put(data)
+                self.output_data_queue.put(data)
+
+            print(datetime.now().strftime("%H:%M:%S.%f"))
--- server.py
+++ server.py
@@ -5,79 +5,89 @@ import tornado.web
 import tornado.websocket
 import tornado.gen
 from tornado.options import define, options
-import os
-import time
+
+# import os
+# import time
 import multiprocessing
 import serialworker
 import json
-
-define("port", default=8080, help="run on the given port", type=int)
+import configparser
 
 clients = []
 
-input_queue = multiprocessing.Queue()
-output_queue = multiprocessing.Queue()
+input_data_queue = multiprocessing.Queue()
+output_data_queue = multiprocessing.Queue()
+input_control_queue = multiprocessing.Queue()
+output_control_queue = multiprocessing.Queue()
 
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render('index.html')
+        self.render("index.html")
 
 
 class StaticFileHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render('main.js')
+        self.render("main.js")
 
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def open(self):
-        print('new connection')
+        print("new connection")
         clients.append(self)
         print("clients len: ", len(clients))
         self.write_message("connected")
 
     def on_message(self, message):
-        print('tornado received from client: %s' % json.dumps(message))
-        # self.write_message('ack')
-        input_queue.put(message)
+        print("tornado received from client: %s" % json.dumps(message))
+        input_data_queue.put(message)
 
     def on_close(self):
-        print('connection closed')
+        print("connection closed")
         clients.remove(self)
 
 
 # check the queue for pending messages, and rely that to all connected clients
 def checkQueue():
-    if not output_queue.empty():
-        message = output_queue.get()
+    if not output_data_queue.empty():
+        message = output_data_queue.get()
         for c in clients:
             c.write_message(message)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    config = configparser.ConfigParser()
+    config.read("./server.cfg")
+    webPort = config["web_server"]["port"]
     # start the serial worker in background (as a deamon)
-    sp = serialworker.SerialProcess(input_queue, output_queue)
+    sp = serialworker.SerialProcess(
+        input_data_queue,
+        output_data_queue,
+        input_control_queue,
+        output_control_queue,
+        config,
+    )
     sp.daemon = True
     sp.start()
-    tornado.options.parse_command_line()
     app = tornado.web.Application(
         handlers=[
             (r"/", IndexHandler),
-            (r"/static/(.*)", tornado.web.StaticFileHandler, {'path':  './'}),
-            (r"/ws", WebSocketHandler)
+            (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "./"}),
+            (r"/ws", WebSocketHandler),
         ]
     )
     httpServer = tornado.httpserver.HTTPServer(app)
-    httpServer.listen(options.port)
+    httpServer.listen(webPort)
 
     mainLoop = tornado.ioloop.IOLoop.instance()
     # adjust the scheduler_interval according to the frames sent by the serial port
-    scheduler_interval = 10
+    scheduler_interval = 100
     scheduler = tornado.ioloop.PeriodicCallback(
-        checkQueue, scheduler_interval, io_loop=mainLoop)
+        checkQueue, scheduler_interval, io_loop=mainLoop
+    )
 
-    url = "http://127.0.0.1:" + str(options.port)
-    print("Listening on " + url)
+    url = "http://127.0.0.1:" + str(webPort)
+    print("Listening on ", url)
     webbrowser.open(url)
 
     scheduler.start()
